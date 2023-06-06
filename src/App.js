5c40260be01b1afa9de6004f1bbf87dd1a9743c2
Albert Arrieta <albert.arrieta@uleth.ca>
2023-06-05 21:28:34 -0600

    hero layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/common/Navbar';
+import HeroSection from './components/sections/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,18 @@
+export const HeroSection = () => {
+    return (
+        <div className='bg-black'>
+            <div className='grid grid-cols-8 container mx-auto py-9 text-left'>
+                <div className="col-span-3">
+                    <h1 className='text-white text-8xl font-semibold'>Engineering Design<span className='text-primary'> & </span>Drafting</h1>
+                </div>
+                <div></div>
+                <div className='col-span-4 bg-white ml-5'>
+                    <p>We are your remote Engineering Design and Drafting Team, offering project cost without overhead expenses.</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
