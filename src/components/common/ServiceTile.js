85b155acbdf7d16c3ddea9309b065aee4f5b33cf
Albert Arrieta <albert.arrieta@uleth.ca>
2023-06-22 20:36:04 -0600

    service tweaks

--- src/components/common/ServiceTile.js
+++ src/components/common/ServiceTile.js
@@ -6,15 +6,23 @@ export const ServiceTile = ({icon, title, paragraph}) => {
 
     const [isHovered, setIsHovered] = useState(false);
 
-    const handleHover = () => {
-      setIsHovered(!isHovered);
+    const hoverOn = () => {
+      setIsHovered(true);
     };
 
+    const hoverOff = () => {
+        setIsHovered(false);
+      };
+
     return (
-        <div className="text-left py-16 px-16 bg-ultrablack border-black border-2 flex-grow" onMouseEnter={handleHover} onMouseLeave={handleHover}>
-            <div className='fill-primary w-20 p-2'>{icon}</div>
+        <div className="text-left h-80 py-16 px-16 bg-ultrablack border-black border-2 flex-1" onMouseEnter={hoverOn} onMouseLeave={hoverOff}>
+            <div>
+                <div className='fill-primary p-2'>{icon}</div>
+            </div>
+            {!isHovered ?  
             <h3 className='text-white text-4xl font-semibold'>{title}</h3>
-            {isHovered && <p className='text-white'>This paragraph appears on hover.</p>}
+            : 
+            <p className='text-white'>{paragraph}</p>}
         </div>
     )
 }
--- src/components/sections/ServicesSection.js
+++ src/components/sections/ServicesSection.js
@@ -4,13 +4,29 @@ import {ReactComponent as DraftIcon } from "../../icons/drafting.svg"
 import {ReactComponent as CostIcon } from "../../icons/cost.svg"
 
 export const ServiceSection = () => {
+
+    const services = [
+        { 
+            title: "Engineering Design",
+            paragraph: "We are a team of professional Mechanical Designers with extensive experience in Oil & Gas, Power Generation and various equipment fabrication. Our main goal is to provide the highest quality mechanical design and drawing package on schedule and on budget."
+        },
+        {
+            title: "Drafting Services",
+            paragraph: ""
+        },
+        {
+            title: "Cost Effectiveness",
+            paragraph: ""
+        }
+    ]
+
     return(
         <div className='bg-ultrablack py-20'>
             <h1 className='text-white text-5xl font-semibold'>Our Services</h1>
-            <div className='container mx-auto flex flex-row gap-1 pt-10'>
-                <ServiceTile title='Engineering Design' icon={<EngIcon />}/>
-                <ServiceTile title='Drafting Services' icon={<DraftIcon />} />
-                <ServiceTile title='Cost Effectiveness' icon={<CostIcon />} />
+            <div className='container mx-auto flex flex-row gap-10 pt-10'>
+                <ServiceTile title={services[0].title} paragraph={services[0].paragraph} icon={<EngIcon className="w-20"/>}/>
+                <ServiceTile title={services[1].title} icon={<DraftIcon className="w-20"/>} />
+                <ServiceTile title={services[2].title} icon={<CostIcon className="w-16"/>} />
             </div>
 
         </div>
